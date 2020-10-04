d5008e681980d66dc6b3a93ab5132081c839325a
ayishafc <39967314+ayishafc@users.noreply.github.com>
2020-10-04 00:10:12 -0400

    Title screen UI and tutorial, intro, level1 scenes created

---
+++ Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Script");
+    }
+
+    // Update is called once per frame
+    public void SetScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
